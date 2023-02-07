6248505737dceadb23fe35d64a162771470a9bd9
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-02-07 13:46:00 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,12 +1,25 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get("https://fr.wikipedia.org/wiki/Beautiful_Soup").text
+url = "https://www.millenium.org/guide/391312.html"
 
-soup = BeautifulSoup(response, features="lxml")
+# Effectuer une demande GET sur la page web
+response = requests.get(url)
 
-random_title = soup.find("h1", class_="firstHeading")
-print("The title of your random article is: " + random_title.text)
+# Vérifier que la requête a réussi
+if response.status_code == 200:
+    # Analyser le contenu de la page web avec BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
 
-with open("data.txt", "a") as file:
-    file.write(random_title.text + "\n")
+    # Rechercher toutes les balises img
+    images = soup.find_all("img")
+
+    # Ouvrir un nouveau fichier texte pour stocker les URLs
+    with open("image_urls.txt", "w") as file:
+        # Boucle sur les images trouvées et écrire leur URL dans le fichier texte
+        for image in images:
+            file.write(image["src"] + "\n")
+
+    print("Les URLs des images ont été enregistrées dans le fichier image_urls.txt")
+else:
+    print("La requête a échoué avec le code d'état", response.status_code)
